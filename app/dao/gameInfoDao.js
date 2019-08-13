013f92b2c06404634be8d82770375e2e56116b95
dujinle <yingyongdujinle@163.com>
2019-08-14 00:36:52 +0800

    update for gameInfoDao

--- app/dao/gameInfoDao.js
+++ app/dao/gameInfoDao.js
@@ -28,15 +28,15 @@ gameInfoDao.get_buy_fangka_list = function(player_id,index,length,cb){
 };
 
 gameInfoDao.get_game_history_list = function(player_id,index,length,cb){
-	var sql = 'select * from game_history where player_id = ? order by creat_time limit ?,?';
+	var sql = 'select * from game_history where location1 = ? or location2 = ? or location3 = ? or location4 = ? order by creat_time limit ?,?';
 
-	var args = [player_id,index,index + length];
+	var args = [player_id,player_id,player_id,player_id,index,index + length];
 	pomelo.app.get('dbclient').query(sql, args, function (err, res) {
 		if (err !== null) {
 			console.log(err.message);//ER_DUP_ENTRY: Duplicate entry '' for key 'INDEX_ACCOUNT_USERNAME'
 			utils.invokeCallback(cb, err, null);
 		} else {
-			console.log('gameInfoDao creater by imei ok  user:' + JSON.stringify(res));
+			console.log('gameInfoDao get_game_history_list:',JSON.stringify(res));
 			utils.invokeCallback(cb,null,res);
 		}
 	});
